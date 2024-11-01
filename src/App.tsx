a404cc1d6db18c4120b455a633b95e7d8ebd6a20
ruvice <andrewwee97@gmail.com>
2024-11-01 23:24:46 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [helixToken, setHelixToken] = useState("")
+  const [clientId, setClientId] = useState("")
+  const [userId, setUserId] = useState("")
+  const [token, setToken] = useState("")
+  const [channelId, setChannelId] = useState("")
+  
+  // Initial setup
+  useEffect(() => {
+    window.Twitch.ext.onAuthorized(function(auth) {
+      setHelixToken(auth.helixToken)
+      setUserId(auth.userId)
+      setClientId(auth.clientId)
+      setToken(auth.token)
+      setChannelId(auth.channelId)
+    })
+    console.log(window.Twitch.ext.version)
+  }, [])
+
+  const handleClick = async (itemName: string) => {
+    try {
+      console.log("Sending message")
+      const response = await fetch(`http://localhost:3000/debug/messagev2`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "message": `Chat thinks you should buy ${itemName}`,
+          "channel_id": channelId,
+          "ebs_token": token,
+          "client_id": clientId
+        })
+      })
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } catch(err) {
+      console.log("Failed to send")
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Suggest an item to buy!</p>
+        <button onClick={() => handleClick("Blink Dagger")}>Blink Dagger</button>
+        <button onClick={() => handleClick("BKB")}>BKB</button>
+        <button onClick={() => handleClick("Mantle of Intelligence")}>Mantle of Intelligence</button>
       </header>
     </div>
   );
